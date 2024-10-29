2786c3c00baaeeee1bcc620496b6ef4b9456060a
carlossm907 <U202015274@upc.edu.pe>
2024-10-29 17:29:59 -0500

    feat(properties): property repository created

---
+++ src/main/java/com/propiconnect/inmoshare/properties/infrastructure/persistence/jpa/PropertyRepository.java
@@ -0,0 +1,15 @@
+package com.propiconnect.inmoshare.properties.infrastructure.persistence.jpa;
+
+import com.propiconnect.inmoshare.properties.domain.model.aggregates.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PropertyRepository  extends JpaRepository<Property, Long > {
+    List<Property> findAllByOwnerId(Long ownerId);
+    Optional<Property> findByOwnerIdAndOwnerName(Long ownerId, String ownerName);
+    boolean existsByOwnerIdAndOwnerName(Long ownerId, String ownerName);
+}
