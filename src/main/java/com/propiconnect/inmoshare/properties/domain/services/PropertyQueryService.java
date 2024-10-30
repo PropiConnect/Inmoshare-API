eb5df4435828107e71c011ff9f4b21ca2cbae2ce
teocchiii <u202211212@upc.edu.pe>
2024-10-30 14:49:10 -0500

    fix(properties): changed the return type from List to Optional.

--- src/main/java/com/propiconnect/inmoshare/properties/domain/services/PropertyQueryService.java
+++ src/main/java/com/propiconnect/inmoshare/properties/domain/services/PropertyQueryService.java
@@ -1,12 +1,15 @@
 package com.propiconnect.inmoshare.properties.domain.services;
 
 import com.propiconnect.inmoshare.properties.domain.model.aggregates.Property;
-import com.propiconnect.inmoshare.properties.domain.model.queries.GetPropertiesByIdQuery;
 import com.propiconnect.inmoshare.properties.domain.model.queries.GetPropertiesByOwnerIdQuery;
+import com.propiconnect.inmoshare.properties.domain.model.queries.GetPropertyByIdQuery;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PropertyQueryService {
-    List<Property> handle(GetPropertiesByIdQuery query);
+    //
     List<Property> handle(GetPropertiesByOwnerIdQuery query);
+    Optional<Property> handle(GetPropertyByIdQuery query);
+
 }
