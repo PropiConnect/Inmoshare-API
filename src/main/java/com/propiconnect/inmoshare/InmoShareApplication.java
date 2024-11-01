1045d72ac3277634a5a8c073ff10897be85a113f
teocchiii <u202211212@upc.edu.pe>
2024-11-01 14:05:58 -0500

    chore: merged branches into this one.

--- src/main/java/com/propiconnect/inmoshare/InmoShareApplication.java
+++ src/main/java/com/propiconnect/inmoshare/InmoShareApplication.java
@@ -3,9 +3,10 @@ package com.propiconnect.inmoshare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//merged
 @SpringBootApplication
 @EnableJpaAuditing
+
 public class InmoShareApplication {
 
     public static void main(String[] args) {
